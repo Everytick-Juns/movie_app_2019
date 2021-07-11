b785c5e798ab8ff2e45873e44a9efc2eb1e025bd
Jun seong <6_5ee@naver.com>
2021-07-11 16:44:44 +0900

    A next commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="App">
+    <h1>Hello</h1>
+    <Food 
+    fav="kimchi" 
+    something={true}
+    papapa={["hello",1,2,3,4,true]} 
+    />
     </div>
-  );
+    );
+}
+
+function Food(props){
+  console.log(props)
+  return <h1>I like Potato</h1>
 }
 
 export default App;
