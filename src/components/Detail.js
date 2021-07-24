ff3898106af71c36ac4de7a742bd4cc2d8125eaa
edu_code <6_5ee@naver.com>
2021-07-24 10:17:20 +0900

    build

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 
-
 class Detail extends React.Component{
     componentDidMount(){
         const {location, history} = this.props;
@@ -12,12 +11,29 @@ class Detail extends React.Component{
     }
     render() {
         
-        const { location } = this.props;
-        if (location.state) {
-            return <span>{location.state.title}</span>
+        const { location:{
+            state}} = this.props;
+        if (state) {
+            return (
+                <div className="movie">
+                <img src={state.poster} alt={state.title} title={state.title} />
+                <div className="movie__data">
+                  <h3 className="movie__title">{state.title}</h3>
+                  <h5 className="movie__year">{state.year}</h5>
+                  <ul className="movie__genres">
+                    {state.genres.map((genre, index) => (
+                      <li key={index} className="genres__genre">
+                        {genre}
+                      </li>
+                    ))}
+                  </ul>
+                  <p className="movie__summary">{state.summary}</p>
+                </div>
+                </div>
+                )
         }else{
             return null;
         }
     }
 }
-export default Detail;
+export default Detail; 
